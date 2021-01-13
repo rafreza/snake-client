d7392e27e09ea18ed734d5522f022fe82d7864f7
Rafin Reza <rafintokyo@gmail.com>
2021-01-13 20:50:14 +0000

    Added message upon succesful connect, and initials

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+const name = 'Name: RR';
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  conn.setEncoding('utf8');
+  conn.on('connect', () => {
+
+    console.log('Connection successfully established!');
+    conn.write(name);
+    
+  });
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+}
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,19 +1,3 @@
-const net = require('net');
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  conn.setEncoding('utf8');
-
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
