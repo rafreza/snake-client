59cdd1b214060ce6fe0dde6a01854f50a52824da
Rafin Reza <rafintokyo@gmail.com>
2021-01-14 21:35:22 +0000

    Added WASD movements based on input

--- input.js
+++ input.js
@@ -1,13 +1,27 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   const handleUserInput = stdin.on('data', (key) => {
-    process.stdout.write(key);
     if (key === '\u0003') {
       process.stdout.write('\n');
       process.exit();
+    }
+    else if (key === 'w') {
+      conn.write('Move: up');
+    }
+    else if (key === 'a') {
+      conn.write('Move: left');
+    }
+    else if (key === 's') {
+      conn.write('Move: down');
+    }
+    else if (key === 'd') {
+      conn.write('Move: right');
     }
   });
   return stdin;
